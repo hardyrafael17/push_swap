96b839c27650e3e88be8f9bcfb2302bb3257e340
Hardy Jimenez Matos <hardy.rafael17@gmail.com>
2022-12-26 23:45:35 +0100

    chore(linux): taking repo to linux
    committing and pushing just to have files available

---
+++ push_swap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hardy.rafael17 <hardy.rafael17@gmail.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/24 00:17:29 by hardy.rafael17    #+#    #+#             */
+/*   Updated: 2022/12/24 00:17:29 by hardy.rafael17   ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <strings.h>
+
+int	main(void)
+{
+	int	test_array[10];
+
+	test_array[0] = 4;
+	test_array[1] = 2;
+	test_array[2] = 9;
+	test_array[3] = 6;
+	test_array[4] = 7;
+	test_array[5] = 3;
+	test_array[6] = 1;
+	test_array[7] = 8;
+	test_array[8] = 5;
+	test_array[9] = 0;
+	return (0);
+}
